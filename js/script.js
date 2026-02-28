e2d35526ffa7f3cf710165bbe97534a47584c09f
hemavathy29 <get2hema2973@gmail.com>
2026-02-28 20:03:33 +0530

    feat: add the autoslide for gallery image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("Reliable Paper Website Loaded");
 
-
+//popup
 function openModal(img) {
     document.getElementById("imageModal").style.display = "block";
     document.getElementById("popupImg").src = img.src;
@@ -9,3 +9,19 @@ function openModal(img) {
 function closeModal() {
     document.getElementById("imageModal").style.display = "none";
 }
+
+// AUTO SLIDE GALLERY
+const track = document.getElementById("galleryTrack");
+let scrollAmount = 0;
+
+function autoSlide() {
+    scrollAmount += 320;
+
+    if (scrollAmount >= track.scrollWidth - track.clientWidth) {
+        scrollAmount = 0;
+    }
+
+    track.style.transform = `translateX(-${scrollAmount}px)`;
+}
+
+setInterval(autoSlide, 3000);
