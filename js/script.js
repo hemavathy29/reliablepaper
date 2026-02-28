ba7825edc1be99d92b0ff4066338fea4bcabcaaf
hemavathy29 <get2hema2973@gmail.com>
2026-02-28 18:20:30 +0530

    feat: add the popup for the gallery image

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Reliable Paper Website Loaded");
+
+
+function openModal(img) {
+    document.getElementById("imageModal").style.display = "block";
+    document.getElementById("popupImg").src = img.src;
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
