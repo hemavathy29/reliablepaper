c4544d839f05c3ac5d4a877514236bd373f3a93c
hemavathy29 <get2hema2973@gmail.com>
2026-03-01 08:29:53 +0530

    fix: gallaery auto-slider

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 console.log("Reliable Paper Website Loaded");
 
-//popup
-function openModal(img) {
-    document.getElementById("imageModal").style.display = "block";
-    document.getElementById("popupImg").src = img.src;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    //popup
+    window.openModal = function (img) {
+        document.getElementById("imageModal").style.display = "block";
+        document.getElementById("popupImg").src = img.src;
+    };
 
-function closeModal() {
-    document.getElementById("imageModal").style.display = "none";
-}
+    window.closeModal= function () {
+        document.getElementById("imageModal").style.display = "none";
+    };
 
-// AUTO SLIDE GALLERY
-const track = document.getElementById("galleryTrack");
-let scrollAmount = 0;
+    // AUTO SLIDE GALLERY
+    const track = document.getElementById("galleryTrack");
+    let scrollAmount = 0;
+    const slideStep = 320; 
 
-function autoSlide() {
-    scrollAmount += 320;
+    function autoSlide() {
+        if(!track) return;
+        scrollAmount += slideStep;
 
-    if (scrollAmount >= track.scrollWidth - track.clientWidth) {
-        scrollAmount = 0;
+        if (scrollAmount >= track.scrollWidth - track.clientWidth) {
+            scrollAmount = 0;
+        }
+
+        track.style.transform = `translateX(-"+ scrollAmount+"px)`;
     }
 
-    track.style.transform = `translateX(-${scrollAmount}px)`;
-}
-
-setInterval(autoSlide, 3000);
+    setInterval(autoSlide, 2500);
+});
